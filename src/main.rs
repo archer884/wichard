21d9e06cdc8349d2f5aecb9d931ac2f3b3b14dc2
J/A <archer884@gmail.com>
2016-04-05 17:23:42 -0500

    use stdin

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod wichard;
 use wichard::Wichard;
 
-use std::io::{Cursor, Read};
+use std::io::Read;
 
 fn main() {
-    let mut reader = Cursor::new("Hello, world! How are you?").wichard();
+    let mut reader = std::io::stdin().wichard();
     let mut buf = String::new();
     
     reader.read_to_string(&mut buf).ok();
